eea81aca91dbaa22e5fc63601ee3f602e4c5fd19
Vathsa <msshrivathsa@gmail.com>
2023-01-15 14:30:43 +0530

    resize and added background

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 // import { IntroScene } from "./IntroScene";
 import { Manager } from "./Manager";
-// import { TitleScene } from "./TitleScene";
-import { Credits } from "./Credits";
-// import { TitleScene } from "./TitleScene";
+import { TitleScene } from "./TitleScene";
 
 Manager.initialize(1920, 1080, 0xcccccc);
 
-Manager.changeScene(new Credits);
+Manager.changeScene(new TitleScene);
