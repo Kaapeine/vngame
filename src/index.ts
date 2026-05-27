be7e40f738c790eb3b8bb70cd7bbe872c894311f
Shrivathsa M S <shrivathsa.ms@solvendo.io>
2026-05-27 18:41:59 +0530

    Removed unused loading code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import { Manager } from "./Manager";
-// import { Scene13 } from "./Scene13";
-// import { LoadingScreen } from "./LoadingScreen";
-import { Scene14 } from "./Scene14";
-// import { TitleScene } from "./TitleScene";
-// import { Sound } from "@pixi/sound";
+import { LoadingScreen } from './LoadingScreen';
+import { Manager } from './Manager';
 
 Manager.initialize(1920, 1080, 0xcccccc);
 
-Manager.changeScene(new Scene14);
-
-// let music: Sound = Sound.from('Music/girlstory_loop1.mp3');
-// music.play()
-// music.loop = true;
+Manager.changeScene(new LoadingScreen());
